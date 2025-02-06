b4db616f29ff5b258a89a08f5a1dbfffcc188b4d
Luuk Nguyen <luuk.nguyen@gmail.com>
2025-02-06 08:16:31 +0100

    created a few things for the bird, just a save commit

--- Assets/Scripts/BirdController.cs
+++ Assets/Scripts/BirdController.cs
@@ -5,21 +5,8 @@ public class BirdController : MonoBehaviour
 {
     private Rigidbody rb;
 
-    private float movementY;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         rb = GetComponent<Rigidbody>();
-    }
-
-    void OnVerticalmovement(InputValue mv)
-    {
-        Vector2 movementVector = mv.Get<Vector2>();
-        movementY = movementVector.y;
-    }
-
-    void FixedUpdate()
-    {
-        rb.AddForce(Vector3.up * 10);
     }
 }
